b9abcbd8317cd513a5eec48f748bb530c1699afa
dnautiyal <deepaknautiyal@hotmail.com>
2022-10-25 13:23:54 -0400

    changed wget to use -nc for weights file download

--- src/train/train_yolov5.sh
+++ src/train/train_yolov5.sh
@@ -13,10 +13,5 @@ weights_filename="yolov5x.pt"
 cfg_filename="yolov5s.yaml"
 # python3 train.py --freeze 74 --batch 16 --epochs 5 --data ../VisDrone/VisDroneData-2019/data.yaml --weights 'yolov7-tiny.pt' --device 0 --cfg ./cfg/training/yolov7-tiny.yaml
 # python3 train.py --freeze 102 --batch 16 --epochs 5 --data ../VisDrone/VisDroneData-2019/data.yaml --weights 'yolov7_training.pt' --device 0 --cfg ./cfg/training/yolov7.yaml
-if [-f $weights_filename]
-then
-    echo "$weights_filename file already exists"
-else
-    wget https://github.com/ultralytics/yolov5/releases/download/v6.2/$weights_filename
-fi
+wget -nc https://github.com/ultralytics/yolov5/releases/download/v6.2/$weights_filename
 python3 train.py --img-size 960 --rect --batch $batch_size --epochs $num_epochs --data ../VisDrone/$dataset_name/data.yaml --weights $weights_filename  --upload_data val
