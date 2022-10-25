e65ab589b556bd2fb61536ff1391223062c2639f
dnautiyal <deepaknautiyal@hotmail.com>
2022-10-25 15:14:27 -0400

    changed train_yolov7 config

--- src/train/train_yolov7.sh
+++ src/train/train_yolov7.sh
@@ -7,9 +7,9 @@ conda activate pytorch
 cd ./yolov7
 # num_freeze_layers=118 # corresponds to yolov7x
 # num_freeze_layers=59 # corresponds to yolov7x backbone
-num_freeze_layers=102 # corresponds to yolov7
-#num_freeze_layers=51 # corresponds to yolov7 backbone
-num_epochs=5
+#num_freeze_layers=102 # corresponds to yolov7
+num_freeze_layers=51 # corresponds to yolov7 backbone
+num_epochs=50
 dataset_name="VisDrone2019-DET-YOLOv7"
 batch_size=24
 # weights_filename="yolov7x_training.pt"
@@ -21,4 +21,4 @@ hyp_filename="yolov7_aerial_detection_hyp.yaml"
 # python3 train.py --freeze 102 --batch 16 --epochs 5 --data ../VisDrone/VisDroneData-2019/data.yaml --weights 'yolov7_training.pt' --device 0 --cfg ./cfg/training/yolov7.yaml
 wget -nc https://github.com/WongKinYiu/yolov7/releases/download/v0.1/$weights_filename
 #python3 train.py --adam --img-size 960 960 --batch $batch_size --epochs $num_epochs --data ../VisDrone/$dataset_name/data.yaml --weights $weights_filename --device 0 --cfg ./cfg/training/$cfg_filename --hyp ../src/train/$hyp_filename
-python3 -m torch.distributed.launch --nproc_per_node 4 train.py --adam --img-size 960 960 --batch $batch_size --epochs $num_epochs --data ../VisDrone/$dataset_name/data.yaml --weights $weights_filename --cfg ./cfg/training/$cfg_filename --hyp ../src/train/$hyp_filename
+python3 -m torch.distributed.launch --nproc_per_node 4 train.py --adam --img-size 960 960 --batch $batch_size --freeze $num_freeze_layers --epochs $num_epochs --data ../VisDrone/$dataset_name/data.yaml --weights $weights_filename --cfg ./cfg/training/$cfg_filename --hyp ../src/train/$hyp_filename
