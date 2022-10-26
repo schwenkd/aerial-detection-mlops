b25c4ad0b9b64ca557072da087eb09857b9e5841
dnautiyal <deepaknautiyal@hotmail.com>
2022-10-26 00:09:41 -0400

    adjusted hyperparameters

--- src/train/train_yolov7.sh
+++ src/train/train_yolov7.sh
@@ -9,7 +9,7 @@ cd ./yolov7
 # num_freeze_layers=59 # corresponds to yolov7x backbone
 #num_freeze_layers=102 # corresponds to yolov7
 num_freeze_layers=51 # corresponds to yolov7 backbone
-num_epochs=100
+num_epochs=300
 dataset_name="VisDrone2019-DET-YOLOv7"
 batch_size=24
 # weights_filename="yolov7x_training.pt"
